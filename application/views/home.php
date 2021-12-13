69768ad9c0a5533a95068549c1b178b8c518f0f1
muchtaragung <muchtaragung7@gmail.com>
2021-12-13 16:11:38 +0700

    hehe

--- application/views/home.php
+++ application/views/home.php
@@ -4795,7 +4795,7 @@
         </div>
     </div>
 
-    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
     <script src="<?= base_url() ?>assets/styles/bootstrap4/popper.js"></script>
     <script src="<?= base_url() ?>assets/styles/bootstrap4/bootstrap.min.js"></script>
     <script src="<?= base_url() ?>assets/plugins/greensock/TweenMax.min.js"></script>
@@ -4806,7 +4806,7 @@
     <script src="<?= base_url() ?>assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
     <script src="<?= base_url() ?>assets/plugins/slick-1.8.0/slick.js"></script>
     <script src="<?= base_url() ?>assets/plugins/easing/easing.js"></script>
-    <script src="js/custom.js"></script>
+    <script src="<?= base_url() ?>assets/js/custom.js"></script>
 </body>
 
 </html>
