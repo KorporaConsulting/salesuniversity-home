7fe0a197ed2e8b3650cf9928521e60f3711a3568
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-12-13 12:55:15 +0700

    data yang perlu di ambil

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,23 +6,17 @@ use Automattic\WooCommerce\Client;
 class Welcome extends CI_Controller
 {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
+
+		/* Data Yang Perlu Di ambil
+		- Kategori
+		- Semua Kelas
+		- Kelas Best Seller
+		- Kelas Per Kategori
+		*/
+
+
 		$woocommerce = new Client(
 			'https://salesuniversity.id',
 			'ck_2f467cee54711ecfa9ba06cc3beb8d8c98aa8e58',
