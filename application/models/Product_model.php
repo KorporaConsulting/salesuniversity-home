d43793b7fe3024eb369ce5a17e17587addde6a0e
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-12-15 13:42:32 +0700

    ambil harga dan juga kategori sukses

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -6,31 +6,41 @@ class Product_model extends CI_Model
     public function get_product()
     {
         $select = '
-        *,
-        posts.id as id,
+        posts.id as id_post,
+        posts.post_title as judul,
+        posts.post_name as slug_post,
+        posts.post_date as tanggal_post,
+        terms.name as kategori,
+        terms.slug as slug_kategori,
+        wc_product_meta_lookup.onsale as diskon,
         GROUP_CONCAT(wpkv_postmeta.meta_key SEPARATOR ",") as meta_key,
         GROUP_CONCAT(wpkv_postmeta.meta_value SEPARATOR ",") as meta_value,
-        posts.post_name as name,
-        posts.post_title as title,
-        wc_product_meta_lookup.onsale as diskon
         ';
 
         $this->db->select($select, false);
-        $this->db->from('wc_product_meta_lookup');
+        $this->db->from('posts');
 
+        // biar di ambil produk woocomerce aja
+        $this->db->join('wc_product_meta_lookup', 'posts.id = wc_product_meta_lookup.product_id');
 
-        $this->db->join('posts', 'posts.id = wc_product_meta_lookup.product_id');
+        // nah price ama saleprice ada disini pokoknya
         $this->db->join('postmeta', 'postmeta.post_id = wc_product_meta_lookup.product_id');
 
-        $this->db->join('term_relationships', 'term_relationships.object_id = posts.id');
+        // ambil kategori
+        $this->db->join('term_relationships', 'posts.id = term_relationships.object_id');
         $this->db->join('terms', 'terms.term_id = term_relationships.term_taxonomy_id');
 
-        $this->db->where(['term_relationships.term_taxonomy_id' => 15]);
+        // ambil yang produk woo saja
+        $this->db->where('terms.term_id != 2');
+        $this->db->where('terms.term_id != 8');
 
-        $this->db->where(['postmeta.meta_key' => '_price']);
-        $this->db->or_where(['postmeta.meta_key' => '_sale_price']);
-        $this->db->or_where(['postmeta.meta_key' => '_regular_price']);
+        // ambil harga reguler ama price
+        $this->db->where('postmeta.meta_key', '_regular_price');
+        $this->db->or_where('postmeta.meta_key', '_sale_price');
+
+        // di grup deh
         $this->db->group_by('postmeta.post_id');
-        return $this->db->get()->result();
+
+        return $this->db->get()->result_array();
     }
 }
