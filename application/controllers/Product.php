8b7696be0287cfc37d6d3f1a3b60df369371a03e
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-12-15 15:23:04 +0700

    hehe

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -9,32 +9,30 @@ class Product extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Product_model','product');
+        $this->load->model('Product_model', 'product');
     }
+
     public function index()
     {
-       $result = $this->product->get_product();
+        $result = $this->product->get_product();
         var_dump($result);
     }
 
-    public function produk()
+    public function all()
     {
-        $woocommerce = new Client(
-			'https://salesuniversity.id',
-			'ck_2f467cee54711ecfa9ba06cc3beb8d8c98aa8e58',
-			'cs_2416fb9fe3138acc4fd5c91e1137956ecc7cc4ee',
-			[
-				'version' => 'wc/v3',
-				'timeout' => 60
-			]
-		);;
+        $result = $this->product->get_product();
+        var_dump($result);
+    }
 
-		// semua kelas
-		$all_product = $woocommerce->get('products', [
-			'page' => 1,
-			'per_page' => 40
-		]);
+    public function by_category()
+    {
+        $result = $this->product->get_product();
+        var_dump($result);
+    }
 
-        var_dump($all_product);
+    public function best_seller()
+    {
+        $result = $this->product->get_product();
+        var_dump($result);
     }
 }
