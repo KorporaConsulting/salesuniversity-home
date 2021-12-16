282439a764238c36dd1944b86204ee993b2c1196
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-12-16 11:04:49 +0700

    by kategoti

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -26,22 +26,21 @@ class Product extends CI_Controller
         $this->load->view('welcome_message');
     }
 
-
-    public function all()
+    public function bestseller()
     {
-        $result = $this->product->get_product();
+        $result = $this->product->get_best_seller_product();
         var_dump($result);
+        $this->load->view('welcome_message');
     }
 
     public function by_category()
     {
-        $result = $this->product->get_product();
-        var_dump($result);
-    }
-
-    public function best_seller()
-    {
-        $result = $this->product->get_product();
+        $result['b2c'] = $this->product->get_product_by_category(46);
+        $result['tanpa kategori'] = $this->product->get_product_by_category(15);
+        // $result['B2B'] = $this->product->get_product_by_category(47);
+        $result['B2B B2C'] = $this->product->get_product_by_category(48);
+        $result['Sales Mindset'] = $this->product->get_product_by_category(49);
+        $result['B2B || B2C || Sales Mindset'] = $this->product->get_product_by_category(50);
         var_dump($result);
     }
 }
