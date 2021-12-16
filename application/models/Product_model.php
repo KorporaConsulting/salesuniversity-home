d6760d2cc5c674a8ae693833cb179c8158afb6c1
Rahmat Gibran Zulbadriansyah <rgibran234@gmail.com>
2021-12-16 11:56:55 +0700

    best seller

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -27,7 +27,6 @@ class Product_model extends CI_Model
         // ambil kategori
         $this->db->join('term_relationships', 'posts.id = term_relationships.object_id');
         $this->db->join('terms', 'terms.term_id = term_relationships.term_taxonomy_id');
-
         // ambil yang produk woo saja
         $this->db->where('terms.term_id != 2');
         $this->db->where('terms.term_id != 8');
@@ -158,6 +157,64 @@ class Product_model extends CI_Model
             $post['sale_price']    = $sale_price;
             $post['regular_price'] = $reg_price;
             $post['img']           = $images;
+            $hasil[$no]            = $post;
+            $no++;
+        }
+
+        return ($hasil);
+    }
+
+    public function get_best_seller_product()
+    {
+        $select = '
+        posts.id as id_post,
+        posts.post_title as judul,
+        posts.post_name as slug_post,
+        posts.post_date as tanggal_post,
+        wc_product_meta_lookup.onsale as diskon,
+        postmeta.meta_value as total_sales
+        ';
+
+        $this->db->select($select, false);
+        $this->db->from('posts');
+
+        // biar di ambil produk woocomerce aja
+        $this->db->join('wc_product_meta_lookup', 'posts.id = wc_product_meta_lookup.product_id');
+
+        $this->db->join('postmeta', 'postmeta.post_id = wc_product_meta_lookup.product_id');
+
+        // ambil kategori
+        $this->db->join('term_relationships', 'posts.id = term_relationships.object_id');
+        $this->db->join('terms', 'terms.term_id = term_relationships.term_taxonomy_id');
+
+
+        $this->db->where('terms.term_id != 2');
+        $this->db->where('terms.term_id != 8');
+        $this->db->where('postmeta.meta_key', 'total_sales');
+
+        $this->db->order_by('cast(wpkv_postmeta.meta_value as int)', 'DESC');
+
+        $datah = $this->db->get()->result();
+        // die;
+        $no = 0;
+        foreach ($datah as $data) {
+
+            $sale_price = $this->get_sale_price($data->id_post);
+            $reg_price  = $this->get_regular_price($data->id_post);
+            $images     = $this->get_images($data->id_post);
+            $kategori   = $this->get_kategori($data->id_post);
+
+            $post['id_post']       = $data->id_post;
+            $post['judul']         = $data->judul;
+            $post['slug_post']     = $data->slug_post;
+            $post['tanggal_post']  = $data->tanggal_post;
+            $post['kategori']      = $kategori->kategori;
+            $post['slug_kategori'] = $kategori->slug_kategori;
+            $post['diskon']        = $data->diskon;
+            $post['sale_price']    = $sale_price;
+            $post['regular_price'] = $reg_price;
+            $post['img']           = $images;
+            $post['total_sales']   = $data->total_sales;
             $hasil[$no]            = $post;
             $no++;
         }
